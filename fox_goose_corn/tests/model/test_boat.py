bef32e0614a3f6943b7aa6aaaf0ab5bb134d9797
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2025-05-10 13:06:22 +0100

    Don't use type hints on the test files

--- fox_goose_corn/tests/model/test_boat.py
+++ fox_goose_corn/tests/model/test_boat.py
@@ -6,32 +6,32 @@ from fox_goose_corn.src.model.boat import (
     TooManyCargoItemsException,
     InvalidCargoItemException,
 )
-from fox_goose_corn.src.model.cargo_item import Fox, Goose, AbstractCargoItem, Corn
+from fox_goose_corn.src.model.cargo_item import Fox, Goose, Corn
 from fox_goose_corn.src.model.river import RiverSide
 
 
 class TestBoat:
     def test_boat_always_starts_the_day_farm_side(self):
-        boat: Boat = Boat()
+        boat = Boat()
 
         assert boat.is_at(RiverSide.FARM_SIDE)
         assert not boat.is_at(RiverSide.MARKET_SIDE)
 
     def test_boat_cannot_cross_from_the_side_its_not_at(self):
-        boat: Boat = Boat()
+        boat = Boat()
 
         with pytest.raises(CannotCrossToExistingSideException):
             boat.cross_from(RiverSide.MARKET_SIDE)
 
     def test_boat_can_cross_river_from_farm_to_market(self):
-        boat: Boat = Boat()
+        boat = Boat()
 
         boat.cross_from(RiverSide.FARM_SIDE)
 
         assert boat.is_at(RiverSide.MARKET_SIDE)
 
     def test_boat_can_cross_river_from_market_to_farm(self):
-        boat: Boat = Boat()
+        boat = Boat()
         boat.cross_from(RiverSide.FARM_SIDE)
 
         boat.cross_from(RiverSide.MARKET_SIDE)
@@ -40,30 +40,30 @@ class TestBoat:
 
     @pytest.mark.parametrize("cargo_item_type", [Fox, Goose, Corn])
     def test_boat_can_take_cargo(self, cargo_item_type):
-        boat: Boat = Boat()
-        cargo_item: AbstractCargoItem = cargo_item_type()
+        boat = Boat()
+        cargo_item = cargo_item_type()
 
         boat.add_cargo(cargo_item)
 
     def test_boat_can_only_take_expected_cargo_types(self):
-        boat: Boat = Boat()
+        boat = Boat()
         cargo_item = Boat()
 
         with pytest.raises(InvalidCargoItemException):
             boat.add_cargo(cargo_item)
 
     def test_boat_cannot_take_more_than_one_cargo_item(self):
-        boat: Boat = Boat()
-        cargo_item_1: AbstractCargoItem = Fox()
-        cargo_item_2: AbstractCargoItem = Goose()
+        boat = Boat()
+        cargo_item_1 = Fox()
+        cargo_item_2 = Goose()
         boat.add_cargo(cargo_item_1)
 
         with pytest.raises(TooManyCargoItemsException):
             boat.add_cargo(cargo_item_2)
 
     def test_boat_can_take_cargo_across_river_from_farm_to_market(self):
-        boat: Boat = Boat()
-        cargo_item: AbstractCargoItem = Fox()
+        boat = Boat()
+        cargo_item = Fox()
 
         boat.add_cargo(cargo_item)
         boat.cross_from(RiverSide.FARM_SIDE)
--- fox_goose_corn/tests/model/test_cargo_item.py
+++ fox_goose_corn/tests/model/test_cargo_item.py
@@ -1,16 +1,16 @@
-from fox_goose_corn.src.model.cargo_item import Fox, AbstractCargoItem
+from fox_goose_corn.src.model.cargo_item import Fox
 from fox_goose_corn.src.model.river import RiverSide
 
 
 class TestCargoItem:
     def test_cargo_always_starts_the_day_farm_side(self):
-        cargo_item: AbstractCargoItem = Fox()
+        cargo_item = Fox()
 
         assert cargo_item.is_at(RiverSide.FARM_SIDE)
         assert not cargo_item.is_at(RiverSide.MARKET_SIDE)
 
     def test_unload_cargo_item_at(self):
-        cargo_item: AbstractCargoItem = Fox()
+        cargo_item = Fox()
 
         cargo_item.unload_cargo_item_at(RiverSide.MARKET_SIDE)
 
