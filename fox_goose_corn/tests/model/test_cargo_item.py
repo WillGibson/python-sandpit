0ee006268ea822834455a16b7adce482ab97a7d6
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2025-05-10 15:02:00 +0100

    Add fox_goose_corn.src.model.cargo_item.AbstractCargoItem.is_on_same_side_as

--- fox_goose_corn/tests/model/test_cargo_item.py
+++ fox_goose_corn/tests/model/test_cargo_item.py
@@ -15,3 +15,16 @@ class TestCargoItem:
         cargo_item.unload_cargo_item_at(RiverSide.MARKET_SIDE)
 
         assert cargo_item.is_at(RiverSide.MARKET_SIDE)
+
+    def test_is_on_same_side_as_with_items_on_same_side_returns_true(self):
+        cargo_item_1 = Fox()
+        cargo_item_2 = Fox()
+
+        assert cargo_item_1.is_on_same_side_as(cargo_item_2)
+
+    def test_is_on_same_side_as_with_items_on_different_sides_returns_false(self):
+        cargo_item_1 = Fox()
+        cargo_item_2 = Fox()
+        cargo_item_2.unload_cargo_item_at(RiverSide.MARKET_SIDE)
+
+        assert not cargo_item_1.is_on_same_side_as(cargo_item_2)
