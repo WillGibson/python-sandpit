9829c6a03cfc6a301bf8e827a9ca89f841a07813
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-09-04 13:11:32 +0100

    Some Pagination Seven work done on a work COP session

--- pagination_seven/src/pagination.py
+++ pagination_seven/src/pagination.py
@@ -1,11 +1,19 @@
+from typing import LiteralString
+
 from typeguard import typechecked
 
 
 @typechecked
-def paginate():
-    raise CannotPaginateException
+def paginate(total_pages: int, current_page: int):
+
+    before: range = range(1, current_page)
+    after: range = range(current_page + 1, total_pages + 1)
+
+    return f"{elide_pages(before, total_pages)} ({current_page}) {elide_pages(after, total_pages)}"
 
 
-@typechecked
-class CannotPaginateException(Exception):
-    pass
+def elide_pages(pages: range, total_pages: int) -> str | LiteralString:
+    if len(pages) > 3 and total_pages > 7:
+        return f"{pages[0]} … {pages[-1]}"
+    else:
+        return " ".join(map(str, pages))
--- pagination_seven/tests/test_pagination.py
+++ pagination_seven/tests/test_pagination.py
@@ -1,10 +1,23 @@
-import pytest
-
-from pagination_seven.src.pagination import paginate, CannotPaginateException
+from pagination_seven.src.pagination import paginate
 
 
-class TestThing:
+class TestPagination:
 
-    def test_paginate_raises_cannot_paginate_exception(self):
-        with pytest.raises(CannotPaginateException):
-            paginate()
+    def test_paginate_highlights_current_page(self):
+        assert paginate(total_pages=5, current_page=2) == "1 (2) 3 4 5"
+
+    def test_paginate_shows_seven_pages(self):
+        assert paginate(total_pages=7, current_page=6) == "1 2 3 4 5 (6) 7"
+
+    def test_paginate_elides_pages_before_the_current_page(self):
+        assert paginate(total_pages=8, current_page=5) == "1 … 4 (5) 6 7 8"
+
+    def test_paginate_elides_pages_after_the_current_page(self):
+        assert paginate(total_pages=8, current_page=4) == "1 2 3 (4) 5 … 8"
+
+    def test_paginate_elides_pages_before_and_after_the_current_page(self):
+        assert paginate(total_pages=9, current_page=5) == "1 … 4 (5) 6 … 9"
+
+    # Todo: Next up!
+    # def test_paginate_doesnt_elides_pages_before_if_not_needed(self):
+    #     assert paginate(total_pages=9, current_page=2) == "1 (2) 3 4 5 … 9"
